6ba20c31860311e8268f41ff0b490c84ba8f3d6b
ricky-kiva <ricky.kiva@gmail.com>
2024-12-17 20:52:23 +0700

    feat(src): impl. logger on req. pre-response

--- src/server.js
+++ src/server.js
@@ -1,21 +1,44 @@
+const os = require('os');
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const logger = require('./logger/index');
+const ClientError = require('./exceptions/ClientError');
+
 require('dotenv').config();
 
 const init = async () => {
   const server = Hapi.server({
     host: 'localhost',
     port: 3000,
-  })
+  });
+
+  server.ext('onPreResponse', (req, h) => {
+    const { response } = req;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+
+      return newResponse;
+    }
+
+    logger.info(`userIP=${req.info.remoteAddress}, host=${os.hostname}, method=${req.method}, path=${req.path}, payload=${JSON.stringify(response.source)}`);
+
+    return h.continue;
+  });
 
   await server.register(
-    {
-      plugin: notes,
-    },
-  )
+    { plugin: notes },
+  );
 
   await server.start();
+
+  // eslint-disable-next-line no-console
   console.log(`server start at ${server.info.uri}`);
-}
+};
 
 init();
